4ac5a1506fdd113f1f9a475ce4221902f3dfddf7
cron410 <cron410@gmail.com>
2022-09-13 06:29:02 -0400

    add disk to output (#3)
    Added disk output for creating snapshots.

--- output.tf
+++ output.tf
@@ -8,8 +8,12 @@ output "internal_ip" {
   value       = google_compute_instance_from_template.compute_instance.network_interface.0.network_ip
 }
 
-
 output "disk_size" {
   description = "Size of disk used for NFS"
   value       = var.capacity_gb
+}
+
+output "nfs_disk" {
+  description = "Persistent disk for NFS instance"
+  value       = google_compute_disk.default
 }
